6561001624b0694d83380dd04a8287e12e7dc42d
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2013-07-21 18:27:18 +0200

    device: allow to build libsepol static lib
    required by busybox to build selinux applets
    
    Change-Id: I607d710b99bf496470e5ceb672639ccc5ace3e29

--- Android.mk
+++ Android.mk
@@ -95,3 +95,25 @@ LOCAL_SHARED_LIBRARIES := libsepol
 LOCAL_MODULE_CLASS := EXECUTABLES
 
 include $(BUILD_HOST_EXECUTABLE)
+
+
+### DEVICE LIBS ###
+
+ifeq ($(HAVE_SELINUX),true)
+
+##
+# libsepol.a
+#
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libsepol
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := $(common_includes)
+LOCAL_CFLAGS := $(common_cflags)
+LOCAL_SRC_FILES := $(common_src_files)  $(hostonly_src_files)
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+
+include $(BUILD_STATIC_LIBRARY)
+
+endif # HAVE_SELINUX
+
